977373e263c6131119bfac893ffbe1414bc687bf
Sina Asgari <53373780+Sina-Asgari@users.noreply.github.com>
2024-04-02 16:13:22 +0330

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+![Homepage](Homepage.png)
+
+This is a project that is taught by [Jonas Schmedtmann](https://github.com/jonasschmedtmann) in his awesome [Course](https://www.udemy.com/course/the-ultimate-react-course/?couponCode=ST13MT40224)
+
+in this project, I learned:
+
+- how to use **react-router** and it's **custom hooks**
+- how to use **lazy loading** for better performance
+- how to create **ProtectedRoute**
+- how to use **ContextApi** and **reducers**
+- how to create and use **custom hooks**
+- how to use **Suspense** component
+
+
+
---
+++ src/components/CountryList.jsx
@@ -0,0 +1,33 @@
+import Spinner from "./Spinner";
+import styles from "./CountryList.module.css";
+import CountryItem from "./CountryItem";
+import Message from "./Message";
+import { useCities } from "../contexts/CitiesContext";
+
+/* eslint-disable react/prop-types */
+function CountryList() {
+  const { cities, isLoading } = useCities();
+
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicing on a city on the map" />
+    );
+
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.map((el) => el.country).includes(city.country))
+      return [...arr, { country: city.country, emoji: city.emoji }];
+    else return arr;
+  }, []);
+
+  return (
+    <ul className={styles.countriesList}>
+      {countries.map((country) => (
+        <CountryItem key={country.country} country={country} />
+      ))}
+    </ul>
+  );
+}
+
+export default CountryList;
